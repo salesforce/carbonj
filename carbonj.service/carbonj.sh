bbe07a245d2431365db3d0ede7a0ace558e82c1b
Christian Bayer <christian.bayer@salesforce.com>
2020-10-12 08:45:10 -0400

    WIP startup for running relay on the pod

--- carbonj.service/carbonj.sh
+++ carbonj.service/carbonj.sh
@@ -6,28 +6,44 @@
 # For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 #
 
-DIR=$( dirname "$0" )
+DIR=$( pwd )
+echo $DIR
 
 if [ -f $DIR/config/service.env ]
 then
        source $DIR/config/service.env
 fi
 
-POD_ID=$(</etc/podid)
+if [ -z ${JAVA+x} ]; then
+    JAVA=`type -p java`
+fi
 
-VERSION=$(ls /build/carbonj/carbonj/carbonj*.jar | cut -d '/' -f 5 | cut -d '-' -f 3 | cut -d '.' -f 1,2,3)
-
-LOG_DIR=/app_logs/carbonj
+if [ -z ${LOG_DIR+x} ]; then
+    LOG_DIR=/app_logs/carbonj
+fi
 
 LOG_FILE=$LOG_DIR/carbonj.log
 
-CONFIG_FILE=/etc/carbonj/application.yml
+if [ -d /build/carbonj/carbonj/ ]; then
+  CLASSPATH=/build/carbonj/carbonj
+else
+  CLASSPATH=$DIR
+fi
 
-export AWS_CREDENTIAL_PROFILES_FILE=/etc/aws/credentials
-export AWS_CONFIG_FILE=/etc/aws/config
+if [ -z ${CONFIG_LOCATION+x} ]; then
+  CONFIG_LOCATION=classpath:/application.yml,classpath:/,/etc/carbonj/application.yml,classpath:/config/,classpath:/config/application.properties,classpath:/config/overrides.properties
+fi
+
+if [ -z ${AWS_CREDENTIAL_PROFILES_FILE+x} ]; then
+  export AWS_CREDENTIAL_PROFILES_FILE=/etc/aws/credentials
+fi
+
+if [ -z ${AWS_CONFIG_FILE+x} ]; then
+  export AWS_CONFIG_FILE=/etc/aws/config
+fi
 
 # Build the command
-COMMAND="${JAVA} -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -jar /build/carbonj/carbonj/carbonj-*.jar --logging.file=${LOG_FILE} --spring.config.location=${CONFIG_FILE}"
+COMMAND="${JAVA} -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -cp ${CLASSPATH}:${CLASSPATH}/* com.demandware.carbonj.service.engine.CarbonJServiceMain --logging.file=${LOG_FILE} --spring.config.location=${CONFIG_LOCATION}"
 
 echo -e "Invoking carbonj\n" | tee -a $LOG_FILE
 echo -e $COMMAND | tee -a $LOG_FILE
