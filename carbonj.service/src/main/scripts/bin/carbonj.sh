74c2e330f4a8cc6d532a06da56a42751a3d0b7d3
sholavanalli <sholavanalli@gmail.com>
2021-03-16 16:59:04 -0400

    Use open source carbonj on 1P

--- carbonj.service/src/main/scripts/bin/carbonj.sh
+++ carbonj.service/src/main/scripts/bin/carbonj.sh
@@ -40,7 +40,7 @@ if [ -z ${AWS_CONFIG_FILE+x} ]; then
 fi
 
 # Build the command
-COMMAND="${JAVA} -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -jar ${HOME_DIR}/lib/carbonj*.jar --logging.file.name=${LOG_FILE} --spring.config.location=${CONFIG_FILE}"
+COMMAND="${JAVA} -Xms512m -Xmx2g -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${LOG_DIR} -Duser.dir=/build/carbonj/carbonj  -jar ${HOME_DIR}/lib/carbonj*.jar --logging.file.name=${LOG_FILE} --spring.config.location=${CONFIG_FILE}"
 
 echo -e "Invoking CarbonJ\n" | tee -a $LOG_FILE
 echo -e $COMMAND | tee -a $LOG_FILE
