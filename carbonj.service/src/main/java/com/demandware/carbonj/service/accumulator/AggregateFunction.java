ca7d116d4dfcf97ea4d85cb0758b49df676c4a32
Rithika <varati.rithik@gmail.com>
2023-07-21 16:16:25 -0400

    Fix the log import

--- carbonj.service/src/main/java/com/demandware/carbonj/service/accumulator/AggregateFunction.java
+++ carbonj.service/src/main/java/com/demandware/carbonj/service/accumulator/AggregateFunction.java
@@ -11,6 +11,8 @@ import com.demandware.carbonj.service.engine.DataPoint;
 import java.util.HashMap;
 import java.util.IntSummaryStatistics;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 interface AggregateFunction
 {
