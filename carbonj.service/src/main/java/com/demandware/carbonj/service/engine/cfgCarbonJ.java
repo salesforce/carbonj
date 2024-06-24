260115b012115d9057ccfc1e147f50a996134cff
Peter Xu <peter.z.xu.crm@gmail.com>
2024-06-24 09:13:55 -0400

    W-15531319: Check null for object to dump stats

--- carbonj.service/src/main/java/com/demandware/carbonj/service/engine/cfgCarbonJ.java
+++ carbonj.service/src/main/java/com/demandware/carbonj/service/engine/cfgCarbonJ.java
@@ -450,13 +450,13 @@ public class cfgCarbonJ
         s.scheduleWithFixedDelay( () -> {
             if (a != null) a.dumpStats();
             if (pbl != null) pbl.dumpStats();
-            qbl.dumpStats();
+            if (qbl != null) qbl.dumpStats(); // Check
             if (pal != null) pal.dumpStats();
-            r.dumpStats();
+            if (r != null) r.dumpStats(); // Check
             if (auditLog != null) auditLog.dumpStats();
             if ( db != null ) db.dumpStats();
-            nettyServer.dumpStats();
-            strCache.dumpStats();
+            if (nettyServer != null) nettyServer.dumpStats(); // Check
+            if (strCache != null) strCache.dumpStats(); // Check
             if ( accu != null ) accu.dumpStats();
         }, 60, 60, TimeUnit.SECONDS );
         return null;
