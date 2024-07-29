7418f46c2627ea069bf789eebef366ed9110a35b
Peter Xu <peter.z.xu.crm@gmail.com>
2024-07-29 13:00:50 -0400

    W-16331851: Set DB table format to 5

--- carbonj.service/src/main/java/com/demandware/carbonj/service/db/points/DataPointArchiveRocksDB.java
+++ carbonj.service/src/main/java/com/demandware/carbonj/service/db/points/DataPointArchiveRocksDB.java
@@ -156,7 +156,7 @@ class DataPointArchiveRocksDB
         }
         catch ( Throwable t )
         {
-            Throwables.throwIfUnchecked(t);
+            throw new RuntimeException(t);
         }
 
     }
@@ -334,10 +334,10 @@ class DataPointArchiveRocksDB
                 }
             }
             int batchSize = batch.count();
-            final Timer.Context timerContext = batchWriteTimer.time();
-            db.write(writeOptions, batch);
-            savedRecordsMeter.mark( batchSize );
-            timerContext.stop();
+            try (Timer.Context ignored = batchWriteTimer.time()) {
+                db.write(writeOptions, batch);
+                savedRecordsMeter.mark(batchSize);
+            }
             return batchSize;
         }
         catch ( RocksDBException e )
@@ -503,6 +503,10 @@ class DataPointArchiveRocksDB
         log.info( "Opening rocksdb '" + dbName + "'. Config options: " + rocksdbConfig );
 
         Options options = new Options().setMaxOpenFiles( -1 );
+        BlockBasedTableConfig cfg = new BlockBasedTableConfig();
+        // TODO: For backward compatibility, should move to latest 6 later
+        cfg.setFormatVersion(5);
+
         if (rocksdbConfig.readOnly) {
             options.setCreateIfMissing(false);
         } else {
@@ -513,11 +517,11 @@ class DataPointArchiveRocksDB
 
             options
                     .setCreateIfMissing(true)
+                    .setCreateMissingColumnFamilies(true)
+                    .setAllowConcurrentMemtableWrite(true)
                     // also check OS config will support this setting
                     .setIncreaseParallelism(rocksdbConfig.increaseParallelism)
-                    .setMaxBackgroundCompactions(rocksdbConfig.maxBackgroundCompactions)
-                    .setMaxBackgroundFlushes(rocksdbConfig.maxBackgroundFlushes)
-                    // .setTableFormatConfig( cfg )
+                    .setMaxBackgroundJobs(rocksdbConfig.maxBackgroundCompactions + rocksdbConfig.maxBackgroundFlushes)
                     // .setArenaBlockSize( )
                     .setMaxWriteBufferNumber(rocksdbConfig.maxWriteBufferNumber)
                     .setWriteBufferSize(rocksdbConfig.writeBufferSize)
@@ -539,7 +543,6 @@ class DataPointArchiveRocksDB
             }
 
             if (rocksdbConfig.useBlockBasedTableConfig) {
-                BlockBasedTableConfig cfg = new BlockBasedTableConfig();
                 if (rocksdbConfig.useBloomFilter) {
                     BloomFilter filter = new BloomFilter(10);
                     cfg.setFilterPolicy(filter);
@@ -548,10 +551,9 @@ class DataPointArchiveRocksDB
                 cfg.setBlockSize(rocksdbConfig.blockSize);
                 cfg.setCacheIndexAndFilterBlocks(true);
                 cfg.setPinL0FilterAndIndexBlocksInCache(true);
-
-                options.setTableFormatConfig(cfg);
             }
         }
+        options.setTableFormatConfig(cfg);
 
         readOptions = new ReadOptions();
         writeOptions = new WriteOptions();
