972c88494f88c3f135e8328d1993a94226eb5ef5
Peter Xu <peter.z.xu.crm@gmail.com>
2024-09-03 12:27:22 -0400

    W-16554282: Add deserializerNameRecord unit test

--- carbonj.service/src/test/java/com/demandware/carbonj/service/db/index/_IndexEntrySerializer.java
+++ carbonj.service/src/test/java/com/demandware/carbonj/service/db/index/_IndexEntrySerializer.java
@@ -10,6 +10,7 @@ import com.demandware.carbonj.service.db.model.RetentionPolicy;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class _IndexEntrySerializer
 {
@@ -72,5 +73,23 @@ public class _IndexEntrySerializer
         assertEquals(e.isLeaf(), a.isLeaf());
         assertEquals(e.getChildren(), a.getChildren());
         assertEquals(e.getRetentionPolicies(), a.getRetentionPolicies());
+    }
+
+    @Test
+    public void deserializerNameRecord() {
+        String key = "pod222.ecom_ag.bjmr.bjmr_prd.search.inventory-support.filter-calculation.filter-update-job.number-of-filters";
+        String hexValue = "01000000029544FEA00000000000000000000300073630733A3234680005356D3A3764000633306D3A3279";
+        byte[] valueBytes = new byte[hexValue.length() / 2];
+        for (int i = 0; i < valueBytes.length; i++) {
+            int index = i * 2;
+            int value = Integer.parseInt(hexValue.substring(index, index + 2), 16);
+            valueBytes[i] = (byte) value;
+        }
+        NameRecord nameRecord = new NameRecordSerializer(true).toIndexEntry(key, valueBytes);
+        assertEquals(key, nameRecord.getKey());
+        assertEquals(11094261408L, nameRecord.getId());
+        assertTrue(nameRecord.getChildren().isEmpty());
+        assertTrue(nameRecord.isLeaf());
+        assertEquals(3, nameRecord.getRetentionPolicies().size());
     }
 }
